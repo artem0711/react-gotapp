f6b93d7340e9fbe7dcc053bc5cdd96b8f6d151bc
Artem Shipovskiy <artem0711@gmail.com>
2021-03-04 13:29:50 +0500

    Refactor header component Remove styled-components from npm dependencies

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,38 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const HeaderBlock = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 80px;
-`;
-
-const HeaderTitle = styled.h3`
-    font-size: 24px;
-    color: #fff;
-    margin: 0;
-`;
-
-const HeaderLinks = styled.ul`
-    display: flex;
-    margin: 0;
-    align-items: center;
-    color: #fff;
-    list-style-type: none;
-    li {
-        margin-right: 20px;
-        font-size: 18px;
-    }
-`;
+import './header.css';
 
 const Header = () => {
     return (
-        <HeaderBlock>
-            <HeaderTitle>
-                <a href="/">Game of Thrones DB</a>
-            </HeaderTitle>
-            <HeaderLinks>
+        <div className="header">
+            <h3><a href="/">Game of Thrones DB</a></h3>
+            <ul className="header-links">
                 <li>
                     <a href="/">Characters</a>
                 </li>
@@ -42,8 +15,8 @@ const Header = () => {
                 <li>
                     <a href="/">Books</a>
                 </li>
-            </HeaderLinks>
-        </HeaderBlock>
+            </ul>
+        </div>
     );
 }
 
